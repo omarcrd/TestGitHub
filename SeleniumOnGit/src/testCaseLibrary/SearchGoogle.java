36a0146f28956b9c15092ab8c6be6808c9207969
omarcrd <omarcrd77@gmail.com>
2016-03-07 17:33:41 -0600

    Add the find and send key statement on the SearchGoogle Class, then call this method on the main method.

--- SeleniumOnGit/src/testCaseLibrary/SearchGoogle.java
+++ SeleniumOnGit/src/testCaseLibrary/SearchGoogle.java
@@ -1,13 +1,15 @@
 package testCaseLibrary;
 
 
+import org.openqa.selenium.By;
+
 import objectLibrary.browserSetUp;
 
 public class SearchGoogle {
 	
 	public static void doSearch(){
 		
-			
+		browserSetUp.driver.findElement(By.id("gs_htif0")).sendKeys("Selenium Tutorial");
 		
 	}
 	
--- SeleniumOnGit/src/testSuite/TestSuiteOne.java
+++ SeleniumOnGit/src/testSuite/TestSuiteOne.java
@@ -1,13 +1,15 @@
 package testSuite;
 
 import objectLibrary.browserSetUp;
+import testCaseLibrary.SearchGoogle;
 
 public class TestSuiteOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		browserSetUp.initializeBrowser();		
+		browserSetUp.initializeBrowser();	
+		SearchGoogle.doSearch();
 		browserSetUp.tearDown();
 
 	}
