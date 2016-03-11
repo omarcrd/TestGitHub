2e05716d5595d4612f11ebd9575fd364a9bdf5c8
omarcrd <omarcrd77@gmail.com>
2016-03-11 09:58:19 -0600

    Update TestSuiteOne.java
    Added a new system.out.println statement to apply new changes on Github so you can fetch to your local.

--- SeleniumOnGit/src/testSuite/TestSuiteOne.java
+++ SeleniumOnGit/src/testSuite/TestSuiteOne.java
@@ -12,6 +12,7 @@ public class TestSuiteOne {
 		SearchGoogle.doSearch();
 		browserSetUp.tearDown();
 		System.out.println("this is a new req I want to implement");
+		System.out.println("This change is applied on Github please fetch those changes to your local and test it");
 
 	}
 
