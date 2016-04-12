b0af071336ce89789073f3191614c079a644c2de
Omar Reyes <omarc@mx1.ibm.com>
2016-04-12 12:27:37 -0500

    set up the firefox profile

--- SeleniumOnGit/src/objectLibrary/browserSetUp.java
+++ SeleniumOnGit/src/objectLibrary/browserSetUp.java
@@ -1,6 +1,8 @@
 package objectLibrary;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class browserSetUp {
@@ -8,16 +10,20 @@ public class browserSetUp {
 	public static WebDriver driver = null;
 	
 	public static void initializeBrowser(){
-		System.setProperty("webdriver.ie.driver", "C://IEDriverServer.exe");
-		driver = new InternetExplorerDriver();		
+		//System.setProperty("webdriver.ie.driver", "C://IEDriverServer.exe");
+		//driver = new InternetExplorerDriver();		
+		
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("security.tls.version.fallback-limit", 1);
+		driver = new FirefoxDriver(profile);
 		driver.manage().window().maximize();
-		driver.get("www.google.com");
+		driver.get("https://www.google.com.mx");
 			
 	}
 	
 	public static void tearDown() throws InterruptedException{
 		Thread.sleep(8000);
-		driver.quit();
+		driver.quit(); //this is latest version branch
 	}
 	
 }
